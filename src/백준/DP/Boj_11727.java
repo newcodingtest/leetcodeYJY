a35e8bd82e96e41a4a5914c99f885d2ce7be6f92
newcodingtest <pulpul8282@naver.com>
2021-11-23 16:01:42 +0900

    백준_11727 dp 2×n 2 타일링

---
+++ src/백준/DP/Boj_11727.java
@@ -0,0 +1,22 @@
+package 백준.DP;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Boj_11727 {
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[1001];
+
+        int n = Integer.parseInt(br.readLine());
+        arr[1]=1;
+        arr[2]=3;
+
+        for (int i=3; i<=n; i++){
+            arr[i]=(arr[i-1]+(2*(arr[i-2])))%10007;
+        }
+        System.out.println(arr[n]);
+
+    }
+}

34845930ca520dc43f7865c1eb6e3d06265788ef
newcodingtest <pulpul8282@naver.com>
2022-01-09 02:02:47 +0900

    백준 2745 n진수 10진수로 변환

---
+++ src/Boj_2745.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Boj_2745 {
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        String target = st.nextToken();
+        int input = Integer.parseInt(st.nextToken());
+
+        int result = 0;
+        for (int i =0; i<target.length(); i++){
+            if('0'<=target.charAt(i) && '9'>=target.charAt(i)){
+                result = result*input + ((int)target.charAt(i)-(int)'0');
+            }else{
+                result = result*input + ((int)target.charAt(i) - (int)'A' + 10);
+            }
+        }
+        System.out.println(result);
+
+    }
+}

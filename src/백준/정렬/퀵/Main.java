99805fd6a91458f451d0721d32e6a85973894e98
newcodingtest <pulpul8282@naver.com>
2022-01-09 22:37:55 +0900

    퀵 정렬

--- src/백준/정렬/퀵/Main.java
+++ src/백준/정렬/퀵/Main.java
@@ -3,6 +3,15 @@ package 백준.정렬.퀵;
 import java.util.Arrays;
 
 // 참고:   https://scshim.tistory.com/267
+/*
+* 대부분의 프로그래밍 언어의 정렬 라이브러리의 근간이 되는 알고리즘
+*
+* 퀵 정렬은 기준을 설정한 다음 큰 수와 작은 수를 교환한 후 리스트를 반으로 나누는 방식
+* 피벗의 위치에 따라서 효율이 극명하게 나뉜다.
+*
+* 현재 방식은 호어 분할 방식을 채택한 것이다.
+* 호어 분할 방식은 리스트에서 첫 번째 데이터를 피벗으로 정한다.
+* */
 public class Main {
     public static void main(String[] args){
 

d0d622ba51a49cabdf07ae8429067fab30f31374
newcodingtest <pulpul8282@naver.com>
2022-01-22 23:59:32 +0900

    queue 이용한 BFS 구현

---
+++ src/백준/그래프검색/DFS/Queue이용한BFS.java
@@ -0,0 +1,53 @@
+package 백준.그래프검색.DFS;
+
+import java.awt.font.FontRenderContext;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Queue이용한BFS {
+    static int[][] list;
+    static boolean[] visited;
+    static StringTokenizer st;
+    static int N, M, start, end;
+
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+
+        list = new int[N+1][N+1];
+        visited = new boolean[N+1];
+
+        for (int i = 0; i<M; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            start = Integer.parseInt(st.nextToken());
+            end = Integer.parseInt(st.nextToken());
+            list[start][end] = 1;
+            list[end][start] = 1;
+        }
+        System.out.print("그래프 방문 순서");
+        bfs(1);
+    }
+
+    static void bfs(int point){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(point);
+        visited[point] = true;
+
+        while(!queue.isEmpty()){
+            int x = queue.poll();
+            System.out.println(x+" ");
+            for (int i=1; i<=N; i++){
+                if (list[x][i]==1 && visited[i]==false){
+                    queue.offer(1);
+                    visited[i]=true;
+                }
+            }
+        }
+
+    }
+
+}

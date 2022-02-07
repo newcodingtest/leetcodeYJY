dcecceafff1d1b3213c6e07fdc37c3dcc76a6748
newcodingtest <pulpul8282@naver.com>
2022-02-07 20:56:00 +0900

    백준 2667 단지 번호 붙이기

---
+++ src/백준/그래프검색/Boj_2667.java
@@ -0,0 +1,62 @@
+package 백준.그래프검색;
+
+public class Boj_2667 {
+    static int N;
+    static int[] x = {-1,1,0,0}; //상하좌우
+    static int[] y = {-0,0,-1,1}; //상하좌우
+    static int [][] map;
+    static boolean [][] visit;
+    static List<Integer> list;
+    static int count = 0;
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        map = new int[N+1][N+1];
+        visit = new boolean[N+1][N+1];
+        list = new ArrayList<Integer>();
+
+        for(int i=0; i<N; i++) {
+            String input = br.readLine();
+            for(int j=0; j<N; j++) {
+                map[i][j]=input.charAt(j)-'0';
+            }
+        }
+
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+                if(map[i][j]==1 && !visit[i][j]) {
+                    count=1;
+                    search(i,j);
+                    list.add(count);
+                }
+            }
+        }
+        Collections.sort(list);
+        System.out.println(list.size());
+        for(int i : list) {
+            System.out.println(i);
+        }
+
+
+    }
+
+    public static void search(int start, int end) {
+        visit[start][end]=true;
+
+        for(int i=0; i<4; i++) {
+            int cX = x[i]+start;
+            int cY = y[i]+end;
+
+            if(cX < N && cY < N && cX>=0 && cY>=0) {
+                if(map[cX][cY]==1 && !visit[cX][cY]) {
+                    search(cX,cY);
+                    count++;
+                }
+            }
+
+        }
+
+
+    }
+}

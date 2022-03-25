5b58ff839b8cabd9b6efc43f6033df462192c22c
newcodingtest <pulpul8282@naver.com>
2022-03-26 07:41:15 +0900

    백준 11047 동전

---
+++ src/백준/그리디/Boj_11047.java
@@ -0,0 +1,33 @@
+package 백준.그리디;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_11047 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int cnt = Integer.parseInt(st.nextToken());
+        int cost = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[cnt];
+
+        for(int i =0; i<cnt; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int coin = 0;
+
+            for(int i =cnt-1; i>=0; i--){
+                if(cost>=arr[i]){
+                    coin += cost / arr[i];
+                    cost = cost % arr[i];
+                }
+            }
+
+        System.out.println(coin);
+    }
+}

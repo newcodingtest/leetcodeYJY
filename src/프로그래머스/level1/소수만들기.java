c3ad20430788dfe624fa8219aa6862b7c63cd147
newcodingtest <pulpul8282@naver.com>
2022-06-23 15:48:33 +0900

    [프로그래머스] 소수만들기

---
+++ src/프로그래머스/level1/소수만들기.java
@@ -0,0 +1,17 @@
+package 프로그래머스.level1;
+
+public class 소수만들기 {
+    public static void main(String[] args){
+        int[] test = {1,2,3,4};
+    }
+
+    public static void 소수(int[] input){
+        for (int i=0; i<input.length; i++){
+            int one = input[i];
+            for (int j=0; j<input.length; j++){
+                int two = [i+1]
+            }
+        }
+
+    }
+}

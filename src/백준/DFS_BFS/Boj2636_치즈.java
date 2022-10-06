82535b16723bf45539d6367f5fcd03ac2e78f2a5
newcodingtest <pulpul8282@naver.com>
2022-10-07 01:16:36 +0900

    [백준] 치즈

--- src/백준/DFS_BFS/Boj2636_치즈.java
+++ src/백준/DFS_BFS/Boj2636_치즈.java
@@ -4,9 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Boj2636_치즈 {
 
@@ -34,59 +32,98 @@ public class Boj2636_치즈 {
                 cheeze[i][j] = Integer.parseInt(str[j]);
             }
         }
+
+        start();
     }
 
-    public static void solve(int seconds){
-        
+
+    public static void start(){
+        int result = 0;
+        int cnt = 0;
+        System.out.println(count());
+        for (int i=0; i<N; i++){
+            for (int j=0; j<M; j++){
+               if(cheeze[i][j]==0 && !visited[i][j]){
+                 int test =   boundarySearch(i,j);
+                   System.out.println("cheeze cnt = " + test);
+                    ++cnt;
+               }
+            }
+            System.out.println("count() = " + count());
+        }
+        System.out.println("seconds = " + cnt);
+    }
+    public static int count(){
+      int cnt = 0;
+        for (int i=0; i<N; i++){
+            for (int j=0; j<M; j++){
+                if(cheeze[i][j]==1){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
     }
 
 
     //경계면 찾아서 1초 제한시간 걸어주기
-    public static void boundarySearch(){
+    public static int boundarySearch(int row, int col){
         Queue<Node> q = new LinkedList<>();
 
-        for (int i=0; i<N; i++){
-            for (int j=0; j<M; j++){
-                if(cheeze[i][j]==0 && !visited[i][j]){
-                    visited[i][j]=true;
-                    q.add(new Node(i,j));
+        q.add(new Node(row,col));
+        visited[0][0]=true;
+
+        int cnt = 0;
+        while(!q.isEmpty()){
+            Node n = q.poll();
+            putTime(n.x, n.y);
+            changeCheezeToAir();
+            ++cnt;
+        }
+        return count();
+    }
+    public static void putTime(int x, int y){
+        for (int i=0; i<4; i++){
+            int nx = rows[i]+x;
+            int ny = cols[i]+y;
+
+            if(nx>=0 && ny>=0 && nx<N && ny<M){
+                if(!visited[nx][ny]){
+                    visited[nx][ny]=true;
+                    if(cheeze[nx][ny]==1){
+                        time[nx][ny]=1;
+                    }else{
+                        putTime(nx,ny);
+                    }
+
                 }
             }
         }
-
-        while(!q.isEmpty()){
-            Node n = q.poll();
-            isValid(n.x, n.y);
-        }
     }
-    
     //1초인 경계면들 공기로 변경
     public static void changeCheezeToAir(){
         for (int i=0; i<N; i++){
             for (int j=0; j<M; j++){
                 if(time[i][j]==1){
-                  cheeze[i][j]=0;
-                  //시간초기화
-                  time[i][j]=0;
+                    changeAir(i,j);
                 }
             }
         }
     }
 
-    public static void isValid(int x, int y){
+    public static void changeAir(int x, int y){
         for (int i=0; i<4; i++){
             int nx = rows[i]+x;
             int ny = cols[i]+y;
 
-            if(nx<0 || ny<0 || nx>N || ny>M){
-                continue;
-            }
-            if(cheeze[nx][ny]==1 && !visited[nx][ny]){
-                visited[nx][ny]=true;
-                time[nx][ny]=1;
+            if(nx>=0 && ny>=0 && nx<N && ny<M){
+                if(cheeze[nx][ny]==1){
+                        cheeze[nx][ny]=0;
+                }
             }
         }
     }
+
 
     static class Node{
         int x;

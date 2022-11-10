194146f6128f337dfa2948063516cdc7affbc2f9
newcodingtest <pulpul8282@naver.com>
2022-11-10 16:52:49 +0900

    [백준] 번데기

--- src/백준/완전탐색/Boj15721_번데기.java
+++ src/백준/완전탐색/Boj15721_번데기.java
@@ -15,39 +15,45 @@ public class Boj15721_번데기 {
         T = Integer.parseInt(br.readLine());
         N = br.readLine();
         makeSentence();
-
         System.out.println(bruteSearch());
     }
 
     public static void makeSentence(){
-     for (int i=4; i<10000; i+=2){
-         // 뻔 - 데 패턴 적용
-         for (int j=0; j<4; j++){
-             if(j%2==0){
-                 sb.append("0");
-             }else{
-                 sb.append("1");
-             }
-             if(sb.length()==10000){
-                 return;
-             }
-         }
-         //그 이후 패턴 적용
-         int temp=i;
-         for(int k=0; i<temp; k++){
-             if(k<temp){
-                 sb.append("0");
-             }else if(k>=temp){
-                 sb.append("1");
-             }
+        while(sb.length()<=10000) {
+            int cnt = 2;
+            // 뻔 - 데 패턴 적용
+            for (int j = 0; j < 4; j++) {
+                if (sb.length() == 10000) {
+                    return;
+                }
+                if (j % 2 == 0) {
+                    sb.append("0");
+                } else {
+                    sb.append("1");
+                }
 
-         }
-     }
+            }
+            //그 이후 패턴 적용
+            for (int i = 0; i < cnt; i++) {
+                if (sb.length() == 10000) {
+                    return;
+                }
+                sb.append("0");
+            }
+            for (int i = 0; i < cnt; i++) {
+                if (sb.length() == 10000) {
+                    return;
+                }
+                sb.append("1");
+            }
+            cnt++;
+        }
+
     }
 
     public static int bruteSearch(){
         int cnt = 0;
-        for (int i=0; i<10000; i++) {
+        for (int i=0; i<=10000; i++) {
             if (String.valueOf(sb.charAt(i)).equals(N)) {
                 cnt++;
             }

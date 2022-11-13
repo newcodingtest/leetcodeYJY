e3414b8df9ddc89a129dc0e32d00e8f90aa8eeb2
newcodingtest <pulpul8282@naver.com>
2022-11-14 02:06:32 +0900

    [백준] 아이스크림

---
+++ src/백준/완전탐색/Boj2422_이탈리아가서아이스크림.java
@@ -0,0 +1,81 @@
+package 백준.완전탐색;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Boj2422_이탈리아가서아이스크림 {
+    static int N,M;
+    static List<Integer>[] list;
+    static boolean[] isVisited;
+    static int way = 0;
+    public static void main(String[] args)throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        list = new List[N+1];
+        isVisited = new boolean[N+1];
+
+        for (int i=1; i<=N; i++){
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i=0; i<M; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            list[x].add(y);
+            list[y].add(x);
+        }
+
+        for (int i=1; i<=N; i++){
+
+            if (!isVisited[i]){
+                isVisited[i]=true;
+                dfs(i,1);
+            }
+
+        }
+
+        System.out.println(way);
+    }
+
+    public static void dfs(int start,int cnt){
+        if(cnt ==3 ){
+            way++;
+            return;
+        }
+
+        System.out.println("start = " + start);
+        for (int i=1; i<=N; i++){
+            if(i==start) {
+                continue;
+            }
+
+            for (int elem : list[start]){
+                isVisited[elem]=true;
+            }
+
+            if(!isVisited[i]){
+                System.out.println("i = " + i);
+                dfs(i,cnt+1);
+                isVisited[i]=false;
+            }
+
+        }
+    }
+
+    public static boolean isValid(int i, int n){
+        for (int elem : list[n]){
+            if(i==elem){
+              return false;
+            }
+        }
+        return true;
+    }
+}

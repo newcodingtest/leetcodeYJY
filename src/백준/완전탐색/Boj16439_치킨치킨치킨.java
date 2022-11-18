5b613dde3138e9a3bf3e894ceedaf552eb7b4057
newcodingtest <pulpul8282@naver.com>
2022-11-18 17:48:15 +0900

    [백준]완탐- 치킨치킨치킨

--- src/백준/완전탐색/Boj16439_치킨치킨치킨.java
+++ src/백준/완전탐색/Boj16439_치킨치킨치킨.java
@@ -3,10 +3,13 @@ package 백준.완전탐색;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Boj16439_치킨치킨치킨 {
     static int N,M;
-    static Map<Integer,Integer> cnt = new HashMap<>();
+    static boolean[] isVisited;
+    static int[][] arr;
+    static int result = 0;
     public static void main(String[] args)throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -14,26 +17,44 @@ public class Boj16439_치킨치킨치킨 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
+        isVisited = new boolean[M];
+        arr = new int[N][M];
 
         for (int i=0; i<N; i++){
             st = new StringTokenizer(br.readLine());
             for (int j=0; j<M; j++){
                 int idx = Integer.parseInt(st.nextToken());
-                cnt.put(j+1,cnt.getOrDefault(j+1,0)+idx);
+                arr[i][j]=idx;
             }
         }
 
-        int []sortedCnt = cnt.entrySet()
-                .stream()
-                .sorted(Map.Entry.comparingByValue((o1, o2) -> {
-                    return o2.intValue()-o1.intValue();
-                }))
-                .mapToInt(value -> value.getKey())
-                .toArray();
+        dfs(0,0);
 
-        Arrays.stream(sortedCnt).forEach(x -> {
-            System.out.print(x);
-        });
+        System.out.println(result);
+    }
 
+    public static void dfs(int start, int deep){
+        if(deep==3){
+            int sum = 0;
+            for (int i=0; i<N; i++){
+                int max = 0;
+                for (int j=0; j<M; j++){
+                    if (isVisited[j]){
+                        max = Math.max(max,arr[i][j]);
+                    }
+                }
+                sum+=max;
+            }
+            result = Math.max(result,sum);
+            return;
+        }
+
+        for (int i=start; i<M; i++){
+            if (!isVisited[i]){
+                isVisited[i]=true;
+                dfs(start+1, deep+1);
+                isVisited[i]=false;
+            }
+        }
     }
 }

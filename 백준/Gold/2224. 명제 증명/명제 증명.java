26989da5583e887660c571c2702f65dd125f8899
HiHello <57785267+newcodingtest@users.noreply.github.com>
2023-07-04 15:03:16 +0900

    [Gold IV] Title: 명제 증명, Time: 176 ms, Memory: 16864 KB -BaekjoonHub

--- 백준/Gold/2224. 명제 증명/명제 증명.java
+++ 백준/Gold/2224. 명제 증명/명제 증명.java
@@ -1,49 +1,70 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    private int atoi(char c) {
-        if (c>='a') return c-'a'+26;
-        return c-'A';
-    }
-    private char itoa(int idx) {
-        if (idx<26) return (char)('A'+idx);
-        return (char)('a'+(idx-26));
-    }
-    private void solution() throws Exception {
+    static int N;
+    static boolean[][] alpabet;
+
+    public static void main(String[] args)throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(br.readLine());
-        boolean[][] arr = new boolean[52][52];
-        while (n-->0) {
-            String cur = br.readLine();
-            arr[atoi(cur.charAt(0))][atoi(cur.charAt(5))] = true;
+
+        N = Integer.parseInt(br.readLine());
+        alpabet = new boolean[52][52];
+
+        for (int i=0; i<N; i++){
+            String str = br.readLine();
+            char start = str.charAt(0);
+            char end = str.charAt(5);
+
+            alpabet[alpabetToInt(start)][alpabetToInt(end)] = true;
         }
-        for (int k = 0; k < 52; k++) {
-            for (int i = 0; i < 52; i++) {
-                for (int j = 0; j < 52; j++) {
-                    if (i==j || i==k || k==j || arr[i][j]) continue;
-                    if (arr[i][k] && arr[k][j]) arr[i][j] = true;
+
+          for (int i=0; i<52; i++){
+            for (int j=0; j<52; j++){
+                for (int k=0; k<52; k++){
+                    if(alpabet[j][k]) continue;
+                    if (j==k || j==i || i==k || alpabet[j][k]) continue;
+                    if(alpabet[j][i] && alpabet[i][k]){
+                        alpabet[j][k]=true;
+                    }
                 }
             }
         }
-        int cnt = 0;
         StringBuilder sb = new StringBuilder();
+             int cnt = 0;
         for (int i = 0; i < 52; i++) {
             for (int j = 0; j < 52; j++) {
-                if (i == j || !arr[i][j]) continue;
-                cnt++;
-                sb.append(itoa(i)).append(' ').append('=').append('>').append(' ').append(itoa(j)).append('\n');
+                if(i==j){continue;}
+                if(alpabet[i][j]){
+                           cnt++;
+                    sb.append(intToAlpabet(i)).append(' ').append('=').append('>').append(' ').append(intToAlpabet(j)).append('\n');
+                }
             }
         }
-        bw.write(String.valueOf(cnt));
-        bw.newLine();
-        bw.write(sb.toString());
-        bw.flush();
+      System.out.println(cnt);
+       System.out.println(sb.toString());
     }
-    public static void main(String[] args) throws Exception {
-        new Main().solution();
+
+    public static int alpabetToInt(char c){
+
+        //대문자
+        if (Character.isUpperCase(c)){
+            return (c-65);
+        }
+        //소문자
+        return (c-'a')+26;
     }
+
+    public static String intToAlpabet(int i){
+        //대문자
+        if(i<26){
+            return Character.toString(65+i);
+        }
+        //소문자
+        return Character.toString(71+i);
+ 
+    }
+
 }
+

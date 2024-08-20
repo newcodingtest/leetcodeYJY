ef24190ca7bb431d6fbb478a91ccd957bdbfaeb3
HiHello <57785267+newcodingtest@users.noreply.github.com>
2024-08-20 15:43:04 +0900

    [level 1] Title: 과일 장수, Time: 102.06 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -1,21 +1,14 @@
 
 import java.util.*;
 class Solution {
-     public static int solution(int k, int m, int[] score) {
+       public static int solution(int k, int m, int[] score) {
         Arrays.sort(score);
         int answer = 0;
 
-        int start = score.length-1;
-        for (int i=0; i<score.length/m; i++){
-            int min = Integer.MAX_VALUE;
-            for (int j = start; j>start-m; j--){
-                min = Math.min(min, score[j]);
-
-            }
-            start-=m;
-            answer+=min*m;
+        for (int i=score.length; i>=m; i-=m){
+            answer+=score[i-m]*m;
         }
-
         return answer;
     }
+
 }

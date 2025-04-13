e7293ebcd5352992322b367f41eb32ff4154f733
HiHello <57785267+newcodingtest@users.noreply.github.com>
2025-04-13 18:49:11 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,40 +1,49 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.*;
+
 
 public class Main {
-	static int[] alpabet;
-	// N개를 입력으로 받아 그룹 단어의 개수를 출력하는 프로그램을 작성하시오
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int[][] alpabet;
 
-		StringBuilder sb = new StringBuilder();
-		int cnt = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		int answer = 0;
-		for (int i=0; i<cnt; i++){
-			String input = br.readLine();
-			if (isGroupWord(input)){
-				answer++;
-			}
-		}
-		System.out.println(	answer);
-	}
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for (int i=0; i<n; i++){
+            String str = br.readLine();
+            alpabet = new int[26][2];
+            if (spellCheck(str)){
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
 
-	public static boolean isGroupWord(String str){
-		alpabet = new int[26];
-		Arrays.fill(alpabet,101);
-		for (int i=0; i<str.length(); i++){
-			if(alpabet[122-str.charAt(i)]==101){
-				alpabet[122-str.charAt(i)]=i;
-			}else{
-				if (alpabet[122-str.charAt(i)]+1==i){
-					alpabet[122-str.charAt(i)]=i;
-				}else{
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+    public static boolean spellCheck(String str){
+        for (int i=0; i<str.length(); i++){
+            //아직 방문 안했을때
+            if (alpabet[str.charAt(i)-'a'][1]==0){
+                alpabet[str.charAt(i)-'a'][0] = i;
+                alpabet[str.charAt(i)-'a'][1] = 1; //방문
+            }
+            //방문 했을때
+            else {
+                //주변에 없다면
+                int distance = Math.abs(i-alpabet[str.charAt(i)-'a'][0]);
+                if (distance>1){
+                    return false;
+                }
+                //주변에 있다면
+                else {
+                    alpabet[str.charAt(i)-'a'][0] = i;
+                }
+            }
+        }
+        return true;
+    }
+
+
 }
+

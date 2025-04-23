e45534973db4b89f8ce9bdc3f1fdf03f125f236b
HiHello <57785267+newcodingtest@users.noreply.github.com>
2025-04-23 15:38:13 +0900

    [level 3] Title: 등굣길, Time: 1.36 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,33 +1,6 @@
-import java.io.IOException;
-
-
 class Solution {
-
-    static int[][] map;
-
-    public static int solution(int m, int n, int[][] puddles) {
-
-        map = new int[n+1][m+1];
-        map[1][1] = 1;
-        for (int[] water : puddles){
-            map[water[1]][water[0]]= -1;
-        }
-        for (int i=1; i<=n; i++){
-            for (int j=1; j<=m; j++){
-                if (map[i][j]==-1){
-                    continue;
-                }
-                if (map[i][j-1]>0){
-                    map[i][j] = (map[i][j]+map[i][j-1])%1_000_000_007;
-                }
-
-                if (map[i-1][j]>0){
-                    map[i][j] = (map[i][j]+map[i-1][j])%1_000_000_007;
-                }
-
-            }
-        }
-
-        return map[n][m];
+    public int solution(int m, int n, int[][] puddles) {
+        int answer = 0;
+        return answer;
     }
 }

195ea643cefcc71925a4705e73d88aa9efcaf89d
HiHello <57785267+newcodingtest@users.noreply.github.com>
2025-09-22 10:58:24 +0900

    [Gold II] Title: 퍼즐, Time: 764 ms, Memory: 94776 KB -BaekjoonHub

--- 백준/Gold/1525. 퍼즐/퍼즐.java
+++ 백준/Gold/1525. 퍼즐/퍼즐.java
@@ -14,55 +14,45 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = null;
-        int[][] map = new int[3][3];
-        int startCol = 0;
-        int startRow = 0;
-
+        StringBuilder sb = new StringBuilder();
         for (int i=0; i<3; i++){
             st = new StringTokenizer(br.readLine());
             for (int j=0; j<3; j++){
-                int elem = Integer.parseInt(st.nextToken());
-                if (elem==0){
-                    startCol = i;
-                    startRow = j;
-                }
-                map[i][j] = elem;
+                sb.append(st.nextToken());
             }
         }
-        System.out.print(bfs(map, startCol, startRow, 0));
+        System.out.print(bfs(sb.toString()));
     }
 
-    private static int bfs(int[][] map, int startCol, int startRow, int cnt) {
-        Set<String> visited = new HashSet<>();
-        Queue<Info> q = new ArrayDeque<>();
+    private static int bfs(String str) {
+        if (str.equals(TARGET)){
+            return 0;
+        }
 
-        q.add(new Info(startCol, startRow, cnt, map));
-        visited.add(toString(map));
+        HashMap<String, Integer> visited = new HashMap<>();
+        visited.put(str, 0);
 
+        Queue<String> q = new ArrayDeque<>();
+        q.add(str);
         while (!q.isEmpty()){
-            Info info = q.poll();
-            int col = info.col;
-            int row = info.row;
-            int[][] nowMap = info.map;
-            int nowCnt = info.cnt;
-
-            String str = toString(nowMap);
-            if (str.equals(TARGET)){
-                return nowCnt;
-            }
+            String cur = q.poll();
+            int idx = cur.indexOf('0');
+            int col = idx/3;
+            int row = idx%3;
 
             for (int i=0; i<4; i++){
                 int ncol = col+moveCol[i];
                 int nrow = row+moveRow[i];
                 if (isBoundary(ncol, nrow)){
+                    String newStr = swap(cur, ncol, nrow, idx);
 
-                    int[][] nextMap = copy(nowMap);
-                    swap(col, row, ncol, nrow, nextMap);
+                    if (cur.equals(TARGET)){
+                        return visited.get(cur);
+                    }
 
-                    String nextStr = toString(nextMap);
-                    if (!visited.contains(nextStr)) {
-                        visited.add(nextStr);
-                        q.add(new Info(ncol, nrow, nowCnt + 1, nextMap));
+                    if (!visited.containsKey(newStr)){
+                        visited.put(newStr, visited.get(cur)+1);
+                        q.add(newStr);
                     }
                 }
             }
@@ -70,30 +60,7 @@ public class Main {
         return -1;
     }
 
-    public static boolean isSimilar(int[][] map){
-        int idx = 1;
-        for (int i=0; i<3; i++){
-            for (int j=0; j<3; j++){
-                if (i==2 && j==2){
-                    if (map[i][j]!=0){
-                        return false;
-                    }
-                } else {
-                    if (map[i][j]!=idx){
-                        return false;
-                    }
-                }
-                idx++;
-            }
-        }
-        return true;
-    }
 
-    public static void swap(int col1, int row1, int col2, int row2, int[][] map){
-        int temp = map[col1][row1];
-        map[col1][row1] = map[col2][row2];
-        map[col2][row2] = temp;
-    }
 
     public static boolean isBoundary(int col, int row){
         if (col<0 || row<0 || col>2 || row>2){
@@ -102,35 +69,14 @@ public class Main {
         return true;
     }
 
-    static class Info {
-        private int col;
-        private int row;
-        private int cnt;
-        private int[][] map;
-
-        public Info(int col, int row, int cnt, int[][] map) {
-            this.col = col;
-            this.row = row;
-            this.cnt = cnt;
-            this.map = map;
-        }
-    }
-
-    public static String toString(int[][] map) {
-        StringBuilder sb = new StringBuilder();
-        for (int[] row : map) {
-            for (int val : row) {
-                sb.append(val);
-            }
-        }
+    public static String swap(String str, int col, int row, int idx1){
+        StringBuilder sb = new StringBuilder(str);
+        int idx2 = (col*3)+row;
+        char temp1 = sb.charAt(idx1);
+        char temp2 = sb.charAt(idx2);
+        sb.setCharAt(idx1, temp2);
+        sb.setCharAt(idx2, temp1);
         return sb.toString();
     }
 
-    public static int[][] copy(int[][] src) {
-        int[][] dest = new int[3][3];
-        for (int i = 0; i < 3; i++) {
-            System.arraycopy(src[i], 0, dest[i], 0, 3);
-        }
-        return dest;
-    }
 }
